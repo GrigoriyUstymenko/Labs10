9b474513f01bb421026366bfe6c810116a346d4b
GrigoriyUstymenko <gromgoriy@gmail.com>
2021-01-06 15:28:34 +0200

    Tasks 1-2

--- Exercises/1-pipe.js
+++ Exercises/1-pipe.js
@@ -1,5 +1,12 @@
 'use strict';
 
-const pipe = (...fns) => x => null;
+const pipe = (...fns) => {
+  for (const fn of fns) {
+    if (typeof fn !== 'function') {
+      throw new Error('All parameters must be functions');
+    }
+  }
+  return x => fns.reduce((v, f) => f(v), x);
+};
 
 module.exports = { pipe };
--- Exercises/2-compose.js
+++ Exercises/2-compose.js
@@ -1,5 +1,29 @@
 'use strict';
 
-const compose = (...fns) => x => null;
+const compose = (...fns) => {
+  const handlers = {};
+  const composed = x => {
+    let res = x;
+    for (const f of fns.reverse()) {
+      try {
+        res = f(res);
+      } catch (err) {
+        composed.emit('error', err);
+        return undefined;
+      }
+    }
+    return res;
+  };
+  composed.on = (name, callback) => {
+    handlers[name] = handlers[name] || [];
+    handlers[name].push(callback);
+  };
+  composed.emit = (name, ...args) => {
+    for (const fn of handlers[name]) {
+      fn(...args);
+    }
+  };
+  return composed;
+};
 
 module.exports = { compose };
